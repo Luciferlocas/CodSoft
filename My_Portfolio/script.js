93eb21a5500b73df83c59f1c709c21efbb3b5695
Luciferlocas <rohitsomvanshi2003@gmail.com>
2023-09-16 11:55:54 +0530

    first commit

--- My_Portfolio/script.js
+++ My_Portfolio/script.js
@@ -1,24 +1,3 @@
-//loading percentage
-
-CSS.registerProperty({
-  name: "--p",
-  syntax: "<integer>",
-  initialValue: 0,
-  inherits: true,
-});
-
-// loading page visibility
-
-document.onreadystatechange = function () {
-  if (document.readyState !== "complete") {
-    document.querySelector("body").style.visibility = "hidden";
-    document.querySelector("#loader").style.opacity= "visible";
-  } else {
-    document.querySelector("#loader").style.display = "none";
-    document.querySelector("body").style.visibility = "visible";
-  }
-};
-
 //typing subheading
 
 var typingEffect = new Typed(".typo", {
